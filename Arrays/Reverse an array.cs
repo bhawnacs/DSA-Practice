b22458c04db5e886da398b1308c40a33d41165ba
Bhawna <bhawnaa.066@gmail.com>
2025-11-04 18:07:21 +0530

    Add files via upload

---
+++ Arrays/Reverse an array.cs
@@ -0,0 +1,49 @@
+﻿/*
+-----------------------------------------------------
+Title: 1D Array Practice - Reverse an Array
+Language: C#
+Author: Bhawna
+Description:
+  This program demonstrates basic operations on 1D Arrays.
+  
+-----------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------//
+            //          REVERSE AN ARRAY             //
+            //---------------------------------------//
+
+
+            int[] numbers = { 45, 46, 85, 88, 77, 66 };
+            int[] reversed = new int[numbers.Length];
+
+            int j = 0;
+
+            for ( int i = numbers.Length - 1; i >= 0; i--)
+            {
+                reversed[j] = numbers[i];
+                j++;
+            }
+            Console.WriteLine("Reversed Array:");
+
+            for ( int i = 0; i < reversed.Length; i++)
+            {
+                Console.WriteLine(reversed[i] + " ");
+            }
+
+            
+        }
+    }     
+}
