8dd9dfc7e91ee7e8362c00a4c9766f10648f99a2
Bhawna <bhawnaa.066@gmail.com>
2025-11-07 17:57:23 +0530

    Add files via upload

---
+++ Arrays/copyArray.cs
@@ -0,0 +1,45 @@
+﻿/*
+--------------------------------------------------------------
+Title: 1D Array Practice - Copy Array
+Language: C#
+Author: Bhawna
+Description:
+  This program demonstrates basic operations on 1D Arrays.
+---------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------//
+            //           COPY ARRAY                  //
+            //---------------------------------------//
+
+
+            int[] numbers = { 45, 46, 85, 88, 77, 66 };
+            int[] copy = new int[numbers.Length];
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                copy[i] = numbers[i];
+            }
+            Console.WriteLine("Copied array element:");
+
+            for(int i = 0; i < copy.Length; i++)
+            {
+                Console.Write(copy[i] + " ");
+            }
+            
+            
+        }
+    }     
+}
