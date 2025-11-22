b3538ef9a18755738a1c6d1857e2e7855b598ba9
Bhawna <bhawnaa.066@gmail.com>
2025-11-22 15:07:57 +0530

    Add files via upload

---
+++ Arrays/secondLargestElement.cs
@@ -0,0 +1,51 @@
+﻿/*
+---------------------------------------------------------------
+Title: 1D Array Practice - Second Largest Element
+Language: C#
+Author: Bhawna
+Description:
+  This program demonstrates operations on 1D Arrays.
+---------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------------//
+            //        Second Largest Element      //
+            //------------------------------------//
+
+            int[] numbers = { 10, 45, 22, 67, 99, 54 };
+
+            int first = int.MinValue;
+            int second = int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > first)
+                {
+                    second = first;
+                    first = numbers[i];
+                }
+                else if (numbers[i] > second && numbers[i] != first)
+                {
+                    second = numbers[i];
+                }
+            }
+
+            Console.WriteLine("Second largest element = " + second);
+
+
+
+        }
+    }     
+}
